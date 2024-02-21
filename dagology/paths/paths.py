50f9fb60819c1ba538d2d6cdc9bdf42428414575
NicolasKozak <nicolas.kozak5@gmail.com>
2024-02-21 12:12:45 +0000

    Compute angle rms deviation

--- dagology/paths/paths.py
+++ dagology/paths/paths.py
@@ -2,6 +2,7 @@ __author__ = "\n".join(["Nicolas Kozak (nicolas.kozak5@gmail.com)"])
 
 import random
 from math import atan
+import numpy as np
 
 def shortest_path(node, weights, distances, predecessors):
     for v, w in weights.items():
@@ -41,10 +42,11 @@ def random_path(node, weights, distances, predecessors):
             distances[random_successor] = new_distance
             predecessors[random_successor] = node
     
-
+######################################################
 # Calculations
+######################################################
 
-def get_angles_relative_to_geodesic(coordinates, weights):
+def get_skewed_angles_minkowski(coordinates, weights):
     """
     Angles are positive when moving away from the geodesic
     and negative when moving towards the geodesic.
@@ -56,3 +58,29 @@ def get_angles_relative_to_geodesic(coordinates, weights):
         sign = 1.0 if x1 == 0.5 or (x1 < 0.5) ^ (x2 > x1) else -1.0
         angles[(u, v)] = sign * atan(abs(x2 - x1) / (t2 - t1))
     return angles
+
+
+def get_angles(coordinates, weights):
+    angles = {}
+    for u, v in weights.keys():
+        t1, x1 = coordinates[u]
+        t2, x2 = coordinates[v]
+        angles[(u, v)] = atan((x2 - x1) / (t2 - t1))
+    return angles
+
+
+def get_angles_rms_deviation_minkowski(coordinates, weights):
+    """
+    Computes angles and rms deviation from +-pi/4.
+    It previously shifts negative angles by pi/2.
+    """
+    angles = get_angles(coordinates, weights)
+    angles_array = np.array(list(angles.values()))
+
+    # Add pi/2 to negative angles
+    angles_array += np.where(angles_array < 0, np.pi/2, 0)
+
+    # Calculate RMS deviation from pi/4
+    rms_deviation = np.sqrt(np.mean((np.pi/4 - angles_array) ** 2))
+
+    return angles, rms_deviation
